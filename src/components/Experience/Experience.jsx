be99eb00ee3c44d5d611d1c934c74cbb4f91f049
chathurkavishmantha <chathurkavish@gmail.com>
2023-02-16 09:42:36 +0530

    fix offer cetion background  color

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
+import React from "react";
 import { Experiences } from "../OurDestinations/DestinationData";
 
 const Experience = () => {
   return (
-    <div className='p-14'>
-    <h2 className="mb-2 text-4xl text-[#104C97] font-[730]">Offers that bring you peace of mind</h2>
-    <div className="pr-8  py-2 text-xl">
+    <div className="p-14">
+      <h2 className="mb-2 text-4xl text-[#104C97] font-[730]">
+        Offers that bring you peace of mind
+      </h2>
+      <div className="pr-8  py-2 text-xl">
         <p>Find the perfect stay with our exclusive deals.</p>
-    </div>
-      <div className="grid grid-cols-3 gap-12">
-        {
-            Experiences.map((item,index)=>{
-                return(                    
-                    <div key={index} className="p-8 bg-cyan-400 relative w-full h-[280px] justify-center items-center">
-                        <img className='absolute top-0 left-0 right-0 bottom-0' src={item.img} alt="" />
-                        <div className='mx-auto text-[#FFFFFF] w-full font-bold uppercase leading-tight py-5 text-2xl absolute left-0 right-0 top-[70%] text-center '>
-                            <p>{item.para}</p>
-                        </div>
-                    </div>
-                )
-            })
-        }
-       
-        
+      </div>
+      <div className="grid grid-cols-3 gap-12 ">
+        {Experiences.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="p-8 bg-cyan-400 relative w-full h-[282px] justify-center items-center "
+            >
+              <img
+                className="absolute  left-0 right-0 bottom-0 w-[100%] h-[100%]"
+                src={item.img}
+                alt=""
+              />
+              <div className="bg-gradient-to-t from-[#0000007a] text-[#FFFFFF]  font-bold uppercase  py-5 text-2xl absolute left-0 right-0 top-0 text-center ">
+                <p className="mt-[210px]">{item.para}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/OurDestinations/OurDestinations.jsx
+++ src/components/OurDestinations/OurDestinations.jsx
@@ -7,20 +7,17 @@ import { DestinationData } from "./DestinationData";
 console.log(DestinationData[0].id);
 
 const OurDestinations = () => {
+  const [data, setData] = useState(DestinationData);
 
-const [data, setData] = useState(DestinationData);
+  const setValues = (index = null) => {
+    DestinationData.forEach((item) => {
+      if (item.id == index) {
+        setData(item);
+      }
+    });
+  };
 
-const setValues = (index = null) =>{
-   DestinationData.forEach(item =>{
-    if(item.id == index){
-        
-        setData(item);        
-    }
-    
-   })
-}
-
-console.log(data);
+  console.log(data);
 
   return (
     <div className="w-[1170px] m-auto">
@@ -38,31 +35,46 @@ console.log(data);
               <option
                 className="w-[50%] text-[20px] font-BodoniModa"
                 key={index}
-                onClick={()=>{setValues(item.id)} }
+                onClick={() => {
+                  setValues(item.id);
+                }}
               >
                 <span>{item.topic}</span>
               </option>
             );
           })}
         </select>
-      </div>  
+      </div>
 
-          <div className="mt-[70px] relative min-h-[550px]">
+      <div className="mt-[70px] relative min-h-[550px]">
         <div className="absolute top-[30px] left-[70px] z-">
-          <img src={!data.island ? DestinationData[0].island : data.island}/>
+          <img src={!data.island ? DestinationData[0].island : data.island} />
         </div>
         <div className="w-[715px] h-[550px] m-auto">
-          <img className="" src={!data.imgMain ? DestinationData[0].imgMain : data.imgMain}/>
+          <img
+            className=""
+            src={!data.imgMain ? DestinationData[0].imgMain : data.imgMain}
+          />
         </div>
-          
-          <div className="bg-[#FFFFFF] pb-[90px] pt-[60px] px-[40px] text-[17px] w-[340px] shadow-card absolute right-0 top-[85px]">
-            <div className="heading"><h5 className="font-BodoniModa tracking-normal text-[#77328b] italic text-[30px]">{!data.topic ? DestinationData[0].topic : data.topic}</h5></div>
-            <div className="para"><br />{!data.description ? DestinationData[0].description : data.description}</div>
-            <div className="mt-[20px]">
-              <a className="px-[30px] py-[13px] text-[20px] border-0 relative overflow-hidden bg-[#252776] text-[#FFFFFF] font-BodoniModa italic ">Discvover More</a>
-            </div>
+
+        <div className="bg-[#FFFFFF] pb-[90px] pt-[60px] px-[40px] text-[17px] w-[340px] shadow-card absolute right-0 top-[85px] ">
+          <div className="heading">
+            <h5 className="font-BodoniModa tracking-normal text-[#77328b] italic text-[30px]">
+              {!data.topic ? DestinationData[0].topic : data.topic}
+            </h5>
           </div>
-        
+          <div className="para">
+            <br />
+            {!data.description
+              ? DestinationData[0].description
+              : data.description}
+          </div>
+          <div className="mt-[20px]">
+            <a className="px-[30px] py-[13px] text-[20px] border-0 relative overflow-hidden bg-[#252776] text-[#FFFFFF] font-BodoniModa italic ">
+              Discvover More
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
